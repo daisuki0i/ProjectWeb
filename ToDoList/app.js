66752385aa830d7753f1c8dfdc4fe4345974c5e5
daisuki0i <daisuki0i@daisuki0is-MacBook-Pro.local>
2022-12-30 18:43:24 +0700

    first commit

---
+++ ToDoList/app.js
@@ -0,0 +1,27 @@
+let input = document.querySelector('.entered-list');
+let addBtn = document.querySelector('.add-list');
+let tasks = document.querySelector('.tasks');
+
+//add new item
+addBtn.addEventListener('click' , () => {
+    if(input.value.trim() != 0){
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `<p> ${input.value} </p>
+        <div class="item-btn">
+           <i class="fa-sharp fa-solid fa-pen-to-square"></i>
+           <i class="fa-sharp fa-solid fa-xmark"></i>
+        </div>`
+
+        tasks.appendChild(newItem);
+        input.value = '';
+    }else{
+        alert('Please enter a task')
+    }
+})
+
+tasks.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa-xmark')){
+        e.target.parentElement.parentElement.remove();
+    }
+})
